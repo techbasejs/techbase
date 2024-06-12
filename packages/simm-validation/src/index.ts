087ccd4e55acc6550c64ab431dd7a70575e0799c
ninhl <ninhl@runsystem.net>
2024-06-12 13:58:32 +0700

    feat: add is-halfwidth-char validator

--- packages/simm-validation/src/index.ts
+++ packages/simm-validation/src/index.ts
@@ -1,9 +1,11 @@
 export const simmValidation = () => {};
 
 import { isFullWidthChar } from "./jpValidation/is-fullwidth-char";
+import { isHalfWidthChar } from "./jpValidation/is-halfwidth-char";
 
 const jpValidator = {
   isFullWidthChar,
+  isHalfWidthChar,
 };
 
 export default jpValidator;
