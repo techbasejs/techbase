3dd68bfe34cbdf3c13e996a4a23d59bc9f4c3093
rs-minhpc <minhpc@runsystem.net>
2024-06-17 08:10:07 +0700

    [simValidate]feat: add minlenght and maxlenght

--- packages/simm-validation/src/index.ts
+++ packages/simm-validation/src/index.ts
@@ -5,3 +5,12 @@ export const isRequired = (value: number) => {
 
   return false;
 };
+export const checkMinLength = (value: string | any[], minLength: number) => {
+  if (value.length >= minLength) return true;
+  return false;
+};
+
+export const checkMaxLength = (value: string | any[], maxLength: number) => {
+  if (value.length <= maxLength) return true;
+  return false;
+};
