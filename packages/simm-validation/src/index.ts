47d694c0d9440a98660f97b00b6da8b9e8dc8dfd
rs-thanhph <pthanh123zz@gmail.com>
2024-06-19 22:37:08 +0700

    fix: refactor file type validate function

--- packages/simm-validation/src/index.ts
+++ packages/simm-validation/src/index.ts
@@ -1,4 +1,3 @@
-import { fileTypes } from "../utils/file-types";
 export const simmValidation = () => {};
 
 export const isRequired = (value: number) => {
@@ -7,18 +6,4 @@ export const isRequired = (value: number) => {
   return false;
 };
 
-export const isFileTypeAccepct = (file: File, fileFormats: string[] = fileTypes) => {
-  const getFileExtension = (fileName: string): string => {
-    const lastDotIndex = fileName.lastIndexOf('.');
-    if (lastDotIndex === -1 || lastDotIndex === 0) return '';
-    return fileName.slice(lastDotIndex + 1).toLowerCase();
-  };
-
-  const fileExtension = getFileExtension(file.name);
-  return fileFormats.includes(fileExtension);
-};
-
-export const isValidFile = (file: File, maxSize: number = 1024*1024) => {
-  if (file.size < maxSize) return true;
-  return false;
-};
+export * from "./is-file-type-accept";
