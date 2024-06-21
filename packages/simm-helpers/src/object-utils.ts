005dc85c48f664cd8a02c9259a78ed7e2cd6e689
trongson <trongson18101999@gmail.com>
2024-06-21 16:52:43 +0700

    feat: create function get different value between two object

---
+++ packages/simm-helpers/src/object-utils.ts
@@ -0,0 +1,34 @@
+import { isArray, isEqual, isObject, transform } from "lodash";
+import { PlainObject } from "./types";
+
+/**
+ * Returns a partial object that contains only the properties in the `object` parameter that are different from the corresponding properties in the `base` parameter.
+ *
+ * @param {T} object - The object to compare against the base object.
+ * @param {T} [base] - The base object to compare against the object parameter.
+ * @return {Partial<T>} A partial object that contains only the properties in the `object` parameter that are different from the corresponding properties in the `base` parameter.
+ */
+export function diffValueBetweenTwoObject<T extends PlainObject>(
+  object: T,
+  base?: T,
+): Partial<T> {
+  function changes(object: T, base?: T): Partial<T> {
+    if (!base) return object;
+    return transform(object, (result: Partial<T>, value: unknown, key) => {
+      if (!isEqual(value, base[key])) {
+        if (isArray(value))
+          result[key as keyof T] = value as T[keyof T] | undefined;
+        else {
+          if (value || base[key])
+            result[key as keyof T] =
+              !(value instanceof Date) && isObject(value) && isObject(base[key])
+                ? (changes(value as T, base[key] as T) as
+                    | T[keyof T]
+                    | undefined)
+                : (value as T[keyof T] | undefined);
+        }
+      }
+    });
+  }
+  return changes(object, base);
+}
