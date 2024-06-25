9cd26dc30ef26781374709f3e0af2de4147aa62c
KhoaNDS <nsikhoa@gmail.com>
2024-06-25 18:57:54 +0700

    feat: fix lint

--- packages/simm-validation/src/is-standard-url.ts
+++ packages/simm-validation/src/is-standard-url.ts
@@ -1,6 +1,6 @@
 /**
  * Checks if a given URL string matches a standard URL format including HTTP(S) and FTP protocols.
- * 
+ *
  * @param url - The URL string to validate.
  * @param urlRegex - Optional regular expression to use for validation. Defaults to a regex that matches
  *                   HTTP(S) and FTP URLs with optional 'www' subdomain, domain name, optional port number,
@@ -8,8 +8,8 @@
  * @returns `true` if the URL string matches the specified format, `false` otherwise.
  */
 export const isStandardURL = (
-    url: string,
-    urlRegex = /^(https?|ftp):\/\/(?:www\.)?([\w!#$%&'()*+,./:;=?@[\]~-]+(?:\.[\w!#$&'()*+,./:;=?@[\]~-]*)*\.[a-z]{2,})(?::\d{2,5})?(?:[#/?]\S*)?$/i
+  url: string,
+  urlRegex = /^(https?|ftp):\/\/(?:www\.)?([\w!#$%&'()*+,./:;=?@[\]~-]+(?:\.[\w!#$&'()*+,./:;=?@[\]~-]*)*\.[a-z]{2,})(?::\d{2,5})?(?:[#/?]\S*)?$/i,
 ) => {
-    return urlRegex.test(url);
-}
+  return urlRegex.test(url);
+};
