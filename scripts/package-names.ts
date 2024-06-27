390b1e64dbf1be369b48e3fd8046b02d02c77eb0
duongnt1 <duongnt1@runsystem.net>
2024-06-27 10:16:02 +0700

    feat: add test scripts

---
+++ scripts/package-names.ts
@@ -0,0 +1,10 @@
+export const packageNames = [
+    'simm',
+    'simm-gen-es-tsconfig',
+    'simm-helpers',
+    'simm-mapped-types',
+    'simm-next-auth',
+    'simm-player',
+    'simm-upload',
+    'simm-validation',
+]
