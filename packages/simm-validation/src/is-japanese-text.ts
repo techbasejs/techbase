f53b3d6fa8d2bac76e593a50c2452f470d6d2073
TrungNguyen0101 <tronzinmj213@gmail.com>
2024-07-02 23:21:53 +0700

    feat: change name and fix check regexp

--- packages/simm-validation/src/is-japanese-text.ts
+++ packages/simm-validation/src/is-japanese-text.ts
@@ -1,6 +1,6 @@
 import { JAPANESE_REGEX } from "./utils/regex";
-import { simmCheckRegexp } from "./utils/simm-check-regexp";
+import { checkRegExp } from "./utils/check-regexp";
 
 export const isJapaneseText = (str: string | null): boolean => {
-  return simmCheckRegexp(str, JAPANESE_REGEX);
+  return checkRegExp(str, JAPANESE_REGEX);
 };
