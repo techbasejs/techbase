365da3a89ea9ed0376bfa02c5e49e2081aa93d8a
rs-minhpc <minhpc@runsystem.net>
2024-07-18 11:24:00 +0700

    feat: simvalidate phone

---
+++ packages/simm-validation/src/is-phone.ts
@@ -0,0 +1,13 @@
+export const validatePhone = (phone: string): { isValid: boolean, errorMessage?: string } => {
+  const phoneNumber = phone.replace(/\D/g, ''); 
+  const phoneRegex = /^0([0-9]){9,10}$/;
+  if (!phoneRegex.test(phoneNumber)) {
+      return {
+          isValid: false,
+          errorMessage: "Phone Error: Phone number must start with 0 and be 10 or 11 digits long"
+      };
+  }
+  return {
+      isValid: true
+  };
+};
