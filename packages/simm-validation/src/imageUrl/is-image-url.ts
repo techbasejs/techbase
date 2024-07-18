287f0dc14bdc9c0155ac45547e5c42ad239aa01c
rs-hauvt <hauvt@runsystem.net>
2024-07-18 11:44:47 +0700

    feat: add is-image-url validate

--- packages/simm-validation/src/imageUrl/is-image-url.ts
+++ packages/simm-validation/src/imageUrl/is-image-url.ts
@@ -1,8 +1,21 @@
 export const isImageURL = (str: string | null): boolean => {
-  if (str == null) {
+  if (!str) {
     return false;
   }
 
-  const imageURLFormats = /\.(jpeg|jpg|gif|png|bmp|webp)$/i;
-  return imageURLFormats.test(str);
+  function isValidUrl(str: string) {
+    try {
+      new URL(str);
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  if (isValidUrl(str)) {
+    const imageURLFormats = /\.(jpeg|jpg|gif|png|bmp|webp)$/i;
+    return imageURLFormats.test(str);
+  } else {
+    return false
+  }
 };
--- packages/simm-validation/test/imageUrl/is-image-url.spec.ts
+++ packages/simm-validation/test/imageUrl/is-image-url.spec.ts
@@ -5,8 +5,8 @@ describe("isImageURL", () => {
   test("returns true for valid image URLs", () => {
     const validImageURLs = [
       "https://tinyjpg.com/images/social/website.jpg",
-      "https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_640.png",
       "https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif",
+      "http://142.251.220.110/png-2702691_640.png",
     ];
 
     for (const imageUrl of validImageURLs) {
@@ -26,8 +26,7 @@ describe("isImageURL", () => {
     }
   });
 
-  test("returns false for null or undefined input", () => {
+  test("returns false for null input", () => {
     expect(isImageURL(null)).toBe(false);
-    expect(isImageURL(undefined)).toBe(false);
   });
 });
