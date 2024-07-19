f4dcc59fcf4901744cf84ba430834f207d1b3bab
lapdq <lapdq@runsystem.net>
2024-07-19 13:45:06 +0700

    fix: #update merged config in interceptor
    Conflicts:
    	pnpm-lock.yaml

--- packages/simm-fetch/src/utils/handle-request-success.ts
+++ packages/simm-fetch/src/utils/handle-request-success.ts
@@ -1,12 +1,6 @@
-import Cookies from 'js-cookie';
-//import { executeBeforeRequestHooks } from './hooks';
-import { getContentType, transformRequestData } from './data-handlers';
 import { APIClientConfig } from '../types';
-import { setContentTypeHeader } from './manage-headers';
-import mergeConfigs from './merge-configs';
+import {mergeConfigs} from './merge-configs';
 
 export const handleRequestSuccess = (config: APIClientConfig, clientConfig: APIClientConfig): any => {
-  //Todo merege Config 
-  //Handle Config
-  return config;
+  return mergeConfigs(config, clientConfig);
 };
