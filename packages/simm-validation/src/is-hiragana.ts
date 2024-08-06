1398fd4a17b108878cb685426b79c3cde003e161
KhoaNDS <nsikhoa@gmail.com>
2024-08-06 22:18:38 +0700

    fix: update parameter type and fix comment

--- packages/simm-validation/src/is-hiragana.ts
+++ packages/simm-validation/src/is-hiragana.ts
@@ -4,12 +4,8 @@
  * Hiragana Unicode range: \u3040-\u309F
  *
  * @param {string} input - The string to validate.
- * @param {RegExp} regex - The regex in order to validate.
  * @returns {boolean} - Returns true if the string contains only Hiragana characters, false otherwise.
  */
-export const isHiragana = (
-  input: string,
-  regex: RegExp = /^[\u3040-\u309F]+$/,
-): boolean => {
-  return regex.test(input);
+export const isHiragana = (input: string | null | undefined): boolean => {
+  return typeof input === "string" && /^[\u3040-\u309F]+$/.test(input);
 };
