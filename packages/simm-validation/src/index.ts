2de81f10ca523d07babbf5310de53bcae61b4864
rs-baolh <baolh@runsystem.net>
2024-08-07 17:52:44 +0700

    [FEAT] @techbase/simm-validation: update comment

--- packages/simm-validation/src/index.ts
+++ packages/simm-validation/src/index.ts
@@ -1,4 +1,4 @@
-export * from "./is-phone-number";
+export * from "./is-japanese-phone-number";
 export * from "./is-full-width";
 export * from "./is-full-width-kana";
 export { isBoolean, isEmpty } from "validator";
--- packages/simm-validation/src/is-japanese-phone-number.ts
+++ packages/simm-validation/src/is-japanese-phone-number.ts
@@ -1,3 +1,5 @@
+import isEmpty from "./is-empty";
+
 const defaultRegex = /^(0([1-9]{1}-?[1-9]\d{3}|[1-9]{2}-?\d{3}|[1-9]{2}\d{1}-?\d{2}|[1-9]{2}\d{2}-?\d{1})-?\d{4}|0[789]0-?\d{4}-?\d{4}|050-?\d{4}-?\d{4})$/;
 
 interface PhoneNumberOptions {
@@ -14,14 +16,15 @@ interface PhoneNumberOptions {
  * @param {RegExp} [customRegex] - Optional custom regex pattern to validate the phone number.
  * @returns {boolean} Returns `true` if the value is a valid phone number according to the patterns; `false` otherwise.
  * @example
- *  const result = isPhoneNumber('0123456789');
+ *  const result = isJapanesePhoneNumber('0123456789');
  *  console.log(result); // prints: true
- *  const result2 = isPhoneNumber('090-1234-5678');
+ *  const result2 = isJapanesePhoneNumber('090-1234-5678');
  *  console.log(result2); // prints: true
  */
 
-export const isPhoneNumber = (value: string | number | null | undefined, options: PhoneNumberOptions = {}): boolean => {
-  if (value === 'undefined' || value === null || value === '') return false;
+export const isJapanesePhoneNumber = <T> (value: string | number | null | undefined | T, options: PhoneNumberOptions = {}): boolean => {
+  if (isEmpty(value)) return false;
+
   const { customRegex } = options;
   const newRegex = customRegex || defaultRegex;
   return newRegex.test(String(value));
