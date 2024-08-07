72aefb046920d7c7f22d110599c5423fc4dddf9c
rs-thanhnn <thanhnn@runsystem.net>
2024-08-07 10:01:34 +0700

    feat: add convert to halfwidth helper (#77)

--- packages/simm-helpers/src/index.ts
+++ packages/simm-helpers/src/index.ts
@@ -3,3 +3,4 @@ export * from "./object-to-query-string";
 export * from "./compare-urls";
 export * from "./utils/object-utils";
 export * from "./convert-halfwidth-to-fullwidth";
+export * from "./to-halfwidth";
