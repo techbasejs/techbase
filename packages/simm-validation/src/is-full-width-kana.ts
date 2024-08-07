c721bcb1b31e75a7b6d5f89602cc12040ed7efea
khanhla <khanhla@runsystem.net>
2024-08-07 12:30:50 +0900

    feat: move file

--- packages/simm-validation/src/is-full-width-kana.ts
+++ packages/simm-validation/src/is-full-width-kana.ts
@@ -7,8 +7,7 @@
  * characters, Japanese spaces, and Japanese punctuation marks. Otherwise, returns false.
  */
 export function isFullWidthKana(text: string): boolean {
-    // eslint-disable-next-line unicorn/better-regex, no-irregular-whitespace
-    const fullWidthKanaRegex = /^[ァ-ヺ　ー・]+$/;
-    return fullWidthKanaRegex.test(text);
+  // eslint-disable-next-line unicorn/better-regex, no-irregular-whitespace
+  const fullWidthKanaRegex = /^[ァ-ヺ　ー・]+$/;
+  return fullWidthKanaRegex.test(text);
 }
-  
