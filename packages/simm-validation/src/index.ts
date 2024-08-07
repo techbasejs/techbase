8c37bda207d1ea9fa9ee58f7552fadadaf38713d
rs-baolh <baolh@runsystem.net>
2024-08-07 11:56:25 +0700

    feat: move func to outside src file and fix conflict manual

--- packages/simm-validation/src/index.ts
+++ packages/simm-validation/src/index.ts
@@ -1,4 +1,4 @@
-export * from "./utils/is-phone-number";
+export * from "./is-phone-number";
 export * from "./is-full-width";
 export * from "./is-full-width-kana";
 export { isBoolean, isEmpty } from "validator";
