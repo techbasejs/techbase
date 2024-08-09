285eaa1ffbcffb6fc868f3a281d17f357c108034
rs-tungnv1 <127287909+rs-tungnv1@users.noreply.github.com>
2024-08-09 10:34:23 +0700

    feat: simm-fetch add query config (#90)
    * feat: simm-fetch add query config
    
    * feat: simm-fetch fix comment

--- packages/simm-fetch/src/utils/query-params.ts
+++ packages/simm-fetch/src/utils/query-params.ts
@@ -1,4 +1,5 @@
 import { has } from "lodash";
+import queryString from 'query-string';
 import qs from "qs";
 
 export function buildQueryParams(params: any): string {
@@ -17,18 +18,13 @@ export function buildQueryParams(params: any): string {
 // export function buildQueryParams(params: { [key: string]: any }): string {
 //   return qs.stringify(params, { arrayFormat: "brackets" });
 // }
-
-export function appendQueryParams(
-  url: string | undefined,
-  params: any,
-): string {
-  const queryString = buildQueryParams(params);
-  if (!queryString) {
+  
+export function appendQueryParams(url: string | undefined, params: any, queryConfig: any = {}): string {
+  const query = queryString.stringify({ ...params }, { ...queryConfig } )
+  if (!query) {
     return url as string;
   }
-  return url?.toString().includes("?")
-    ? `${url}&${queryString}`
-    : `${url}?${queryString}`;
+  return url?.toString().includes('?') ? `${url}&${query}` : `${url}?${query}`;
 }
 
 export function objectToQueryString(obj: {
