f4238fecedaab0f95cc07f8dc293dfc585a55a73
rs-hannd <hannd@runsystem.net>
2024-08-13 17:14:14 +0700

    fix: fix review

--- packages/simm-validation/src/is-katakana.ts
+++ packages/simm-validation/src/is-katakana.ts
@@ -5,7 +5,7 @@
  * @return {boolean} Returns true if the input string contains only Katakana characters, false otherwise.
  */
 export function isKatakana(input: string): boolean {
-  if (!input) return false;
+  if (!input) return true;
   // eslint-disable-next-line unicorn/better-regex
   const katakanaRegex = /^[\u30A0-\u30FF\uFF65-\uFF9F\u31F0-\u31FF]+$/;
   return katakanaRegex.test(input);
