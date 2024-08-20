6bdf3a4a369702721eb4c8f97c83b74b66c77f0f
rs-nhatq2 <nhatq1@runsystem.net>
2024-08-20 15:52:00 +0700

    fix: refactor code

--- packages/simm-upload/tests/utils/rename-file.spec.ts
+++ packages/simm-upload/tests/utils/rename-file.spec.ts
@@ -1,29 +1,28 @@
-import { describe, it, expect } from 'vitest';
-import { renameFile } from '../../src/rename-file';
+import { describe, it, expect } from "vitest";
+import { renameFile } from "../../src/rename-file";
 
 interface FileOptions {
   lastModified?: number;
   type?: string;
 }
 
-
 global.File = function (fileBits, fileName, options: FileOptions = {}) {
   this.name = fileName;
   this.lastModified = options.lastModified || Date.now();
-  this.type = options.type || '';
+  this.type = options.type || "";
   this.size = fileBits.reduce((acc, curr) => acc + curr.length, 0);
 
   this.arrayBuffer = async () => new ArrayBuffer(this.size);
-  this.slice = (start, end, contentType) => new Blob(fileBits.slice(start, end), { type: contentType });
+  this.slice = (start, end, contentType) =>
+    new Blob(fileBits.slice(start, end), { type: contentType });
   this.stream = () => new ReadableStream();
-  this.text = async () => fileBits.join('');
+  this.text = async () => fileBits.join("");
 } as any;
 
-describe('simmUpload', () => {
-
-  it('should rename the file without timestamp', () => {
-    const file = new File(['content'], 'oldName.txt', { type: 'text/plain' });
-    const newName = 'newName.txt';
+describe("simmUpload", () => {
+  it("should rename the file without timestamp", () => {
+    const file = new File(["content"], "oldName.txt", { type: "text/plain" });
+    const newName = "newName.txt";
     const newFile = renameFile(file, newName);
 
     expect(newFile.name).toBe(newName);
@@ -31,9 +30,9 @@ describe('simmUpload', () => {
     expect(newFile.lastModified).toBe(file.lastModified);
   });
 
-  it('should rename the file with timestamp', () => {
-    const file = new File(['content'], 'oldName.txt', { type: 'text/plain' });
-    const newName = 'newName.txt';
+  it("should rename the file with timestamp", () => {
+    const file = new File(["content"], "oldName.txt", { type: "text/plain" });
+    const newName = "newName.txt";
     const newFile = renameFile(file, newName, true);
 
     expect(newFile.name.startsWith(newName)).toBe(true);
@@ -42,9 +41,9 @@ describe('simmUpload', () => {
     expect(newFile.lastModified).toBe(file.lastModified);
   });
 
-  it('should not rename the file if hasTimestamp is false', () => {
-    const file = new File(['content'], 'oldName.txt', { type: 'text/plain' });
-    const newName = 'newName.txt';
+  it("should not rename the file if hasTimestamp is false", () => {
+    const file = new File(["content"], "oldName.txt", { type: "text/plain" });
+    const newName = "newName.txt";
     const newFile = renameFile(file, newName, false);
 
     expect(newFile.name).toBe(newName);
