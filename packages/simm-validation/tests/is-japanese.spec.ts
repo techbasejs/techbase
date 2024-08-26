fe3acd1502df25c318bfd326e94e0199489a90d5
TrungNguyen0101 <tronzinmj213@gmail.com>
2024-08-26 21:47:21 +0700

    fix: fix comment

--- packages/simm-validation/tests/is-japanese.spec.ts
+++ packages/simm-validation/tests/is-japanese.spec.ts
@@ -6,44 +6,40 @@ describe("isJapanese", () => {
     const testString = "こんにちは"; // Hiragana
     expect(isJapanese(testString)).toBe(true);
   });
-
   test("returns true for a string containing only Katakana characters", () => {
     const testString = "カタカナ"; // Katakana
     expect(isJapanese(testString)).toBe(true);
   });
-
   test("returns true for a string containing only Full-width Katakana characters", () => {
     const testString = "ｶﾀｶﾅ"; // Full-width Katakana
     expect(isJapanese(testString)).toBe(true);
   });
-
   test("returns true for a string containing only Kanji characters", () => {
     const testString = "漢字"; // Kanji
     expect(isJapanese(testString)).toBe(true);
   });
-
   test("returns true for a string containing mixed Hiragana, Katakana, and Kanji characters", () => {
     const testString = "今日はカタカナと漢字"; // Mixed Hiragana, Katakana, and Kanji
     expect(isJapanese(testString)).toBe(true);
   });
-
-  test("returns false for a string containing mixed Japanese characters", () => {
+  test("returns false for a string containing mixed Japanese characters and others", () => {
     const testString = "今日は、Good morning! おはようございます。";
     expect(isJapanese(testString)).toBe(false);
   });
-
   test("returns false for a string containing non-Japanese characters", () => {
     const testString = "Hello, こんにちは";
     expect(isJapanese(testString)).toBe(false);
   });
-
   test("returns false for an empty string", () => {
     const testString = "";
     expect(isJapanese(testString)).toBe(false);
   });
-
   test("returns false for null", () => {
     const testString = null;
+    expect(isJapanese(testString)).toBe(false);
+  });
+  test("returns false for undefined", () => {
+    const testString = undefined;
     expect(isJapanese(testString)).toBe(false);
   });
 });
