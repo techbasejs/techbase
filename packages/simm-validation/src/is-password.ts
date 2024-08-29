d813441d95b0a9d0f6963764c91cf3f3fdeff931
KhoaNDS <nsikhoa@gmail.com>
2024-08-29 20:58:55 +0700

    fix: update index.ts and update @example

--- packages/simm-validation/src/is-password.ts
+++ packages/simm-validation/src/is-password.ts
@@ -1,10 +1,21 @@
 /**
  * @description check if password has at least 8 characters,
- * contains number characters,
- * contains uppercase characters,
- * contains non-alphanumeric characters
- * @param value string
- * @returns boolean
+ * contains at least number characters,
+ * contains at least uppercase characters,
+ * contains at least non-alphanumeric characters
+ * @param value string - The password string to validate.
+ * @param passwordRegex Regex - Optional custom regular expression for validation.
+ * @returns boolean - Returns true if the password is valid, otherwise false.
+ * @example
+ * // Basic usage
+ * const password = "StrongP@ss1";
+ * const isValid = isPassword(password);
+ * console.log(isValid); // true
+ * 
+ * // Custom regex usage
+ * const customRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[\w@$!%*?&]{10,}$/;
+ * const isCustomValid = isPassword("Custom@Pass123", customRegex);
+ * console.log(isCustomValid); // true
  */
 export const isPassword = (
   value: string,
