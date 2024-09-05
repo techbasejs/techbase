39427d4cf1f21d961f3296e57b75fc7272326f69
rs-hauvt <hauvt@runsystem.net>
2024-09-05 08:31:10 +0700

    feat: fix comment

--- packages/simm-validation/src/is-url-image.ts
+++ packages/simm-validation/src/is-url-image.ts
@@ -6,11 +6,11 @@ import { regexValidate } from "../src/shared/constants/regex";
  * @returns {boolean} True if the string is a valid image URL, otherwise false.
  * @example
  *  const result = isImageURL('https://tinyjpg.com/images/social/website.jpg');
- *  console.log(result); // prints: true
+ *  console.log(result); // true
  *  const result2 = isImageURL('abc');
- *  console.log(result2); // prints: false
+ *  console.log(result2); // false
  *  const result3 = isImageURL(null);
- *  console.log(result3); // prints: false
+ *  console.log(result3); // false
  */
 export const isImageURL = (str: string | null | undefined): boolean => {
   if (!str || !isURL(str)) return false;
