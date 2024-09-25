698420fe0b85254be2880ad4eacb3e318ba7af69
Nguyen Dinh Truong <truongnd1@runsystem.net>
2024-09-25 10:58:40 +0700

    feat: add isMimeType validate

---
+++ packages/simm-validation/tests/is-mime-type.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from "vitest";
+import { isMimeType } from "../src/is-mime-type";
+
+describe("isMimeType", () => {
+  it("should be mime type", () => {
+    expect(isMimeType("image/jpeg")).toBe(true);
+    expect(isMimeType("text/plain; charset=UTF-8")).toBe(true);
+    expect(isMimeType("multipart/form-data; boundary=aBoundaryString")).toBe(
+      true
+    );
+  });
+
+  it("should not be mime type", () => {
+    expect(isMimeType(1234)).toBe(false);
+    expect(isMimeType("foo/bar")).toBe(false);
+    expect(isMimeType("image/jpeg/")).toBe(false);
+  });
+});
