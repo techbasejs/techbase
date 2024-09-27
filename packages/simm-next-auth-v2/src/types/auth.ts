a6e1be2f17fc14e0c844aa438beb301d31ed6b6a
khanhla <khanhla@runsystem.net>
2024-09-27 11:00:37 +0700

    feat: update type

--- packages/simm-next-auth-v2/src/types/auth.ts
+++ packages/simm-next-auth-v2/src/types/auth.ts
@@ -1,22 +1,21 @@
 import { Provider } from "../provider";
 import { AuthRequestType } from "./provider";
 
-type Awaitable<T> = T | PromiseLike<T>
+type Awaitable<T> = T | PromiseLike<T>;
 
 interface AuthConfig {
   providers: Provider<AuthRequestType>[];
-  callbacks?: CallbacksType
+  callbacks?: CallbacksType;
 }
 
 type CallbacksType = {
-  jwt: (token: string) => string,
-  session: (token: string) => string,
+  jwt?: (token: string) => string;
+  session?: (token: string) => string;
   redirect?: (params: {
-    url: string
-    baseUrl: string,
-  }) => Awaitable<string>
-}
-export {
-  AuthConfig,
-  CallbacksType
-}
+    url: string;
+    baseUrl: string;
+    user: Record<string, number | string | undefined | null | boolean | object>;
+  }) => Awaitable<string>;
+};
+
+export { AuthConfig, CallbacksType };
