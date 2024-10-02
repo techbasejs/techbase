db7ea0a9d8f4e77970b3876356d968b1a26d1f7f
khanhla <khanhla@runsystem.net>
2024-10-02 17:31:44 +0700

    feat: fix comment

--- packages/simm-next-auth-v2/src/constants.ts
+++ packages/simm-next-auth-v2/src/constants.ts
@@ -1,9 +1,5 @@
-const COOKIE_DATA_KEY = "__d";
-
-const COOKIE_TOKEN_KEY = "__token";
-
 enum AUTH_PROVIDER {
   CREDENTIALS = "credentials",
 }
 
-export { COOKIE_DATA_KEY, COOKIE_TOKEN_KEY, AUTH_PROVIDER };
+export { AUTH_PROVIDER };
