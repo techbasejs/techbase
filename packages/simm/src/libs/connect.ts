9dfe51f995227a52412bccce1db90d30e23c40f1
rs-hannd <hannd@runsystem.net>
2024-10-10 16:19:31 +0700

    feat: multi-level jump proxy

--- packages/simm/src/libs/connect.ts
+++ packages/simm/src/libs/connect.ts
@@ -2,6 +2,50 @@ import { Client } from "ssh2";
 import consola from "consola";
 import { SimmConfig } from "../types";
 
+function connectToNextServer(
+  simmConfig: SimmConfig,
+  client: Client,
+  jumpConfig: any,
+  serverConfig: any,
+  onReady: (client: Client, otherClient?: Client) => void,
+  prevClient?: Client,
+) {
+  client.on("ready", () => {
+    if (serverConfig.proxyJump) {
+      const nextJumpConfig = simmConfig?.servers?.[serverConfig.proxyJump];
+      if (!nextJumpConfig) {
+        consola.error(`proxyJump ${serverConfig.proxyJump} is not defined`);
+        return;
+      }
+
+      consola.info(`Jumping to server ${nextJumpConfig.host}...`);
+      client.forwardOut(
+        "127.0.0.1",
+        8124,
+        nextJumpConfig.host as string,
+        nextJumpConfig.port || 22,
+        (err, stream) => {
+          if (err) {
+            consola.error(`Error jumping to server ${nextJumpConfig.host}`);
+            throw err;
+          }
+          const nextClient = new Client();
+          connectToNextServer(simmConfig, nextClient, nextJumpConfig, serverConfig, onReady, client);
+        },
+      );
+    } else {
+      consola.success(`Connected to final server ${serverConfig.host}`);
+      onReady(client, prevClient);
+    }
+  });
+
+  client.connect({
+    ...jumpConfig,
+    agentForward: true,
+    agent: process.env.SSH_AUTH_SOCK,
+  });
+}
+
 export function sshConnect(
   simmConfig: SimmConfig,
   environment: string,
@@ -9,49 +53,19 @@ export function sshConnect(
 ) {
   const client = new Client();
   const serverConfig = simmConfig?.servers?.[environment];
-  if (serverConfig) {
-    const jumpClient = new Client();
-    if (serverConfig.proxyJump) {
-      const jumpConfig = simmConfig?.servers?.[serverConfig.proxyJump];
-      consola.info(`Connecting to server ${jumpConfig.host}...`);
-      if (!jumpConfig) {
-        consola.info("proxyJump is not defined");
-        return;
-      }
-      client
-        .on("ready", () => {
-          client.forwardOut(
-            "127.0.0.1",
-            8124,
-            serverConfig.host as string,
-            serverConfig.port || 22,
-            (err, stream) => {
-              if (err) {
-                throw err;
-              }
-              consola.info(`Jumping to server ${serverConfig.host}...`);
-              jumpClient.connect({
-                sock: stream,
-                ...serverConfig,
-              });
-            },
-          );
-        })
-        .connect({
-          ...jumpConfig,
-          agentForward: true,
-          agent: process.env.SSH_AUTH_SOCK,
-        });
+  if (serverConfig && serverConfig.proxyJump) {
+    const initialJumpConfig = simmConfig?.servers?.[serverConfig.proxyJump];
+    if (initialJumpConfig) {
+      consola.info(`Connecting to initial jump server ${initialJumpConfig.host}...`);
+      connectToNextServer(simmConfig, client, initialJumpConfig, serverConfig, onReady);
     } else {
-      consola.info(`Connecting to server ${serverConfig.host}...`);
-      jumpClient.connect({
+      consola.info(`Connecting directly to server ${serverConfig.host}...`);
+      client.connect({
         ...serverConfig,
       });
+      client.on("ready", () => onReady(client));
     }
-    jumpClient.on("ready", () => {
-      onReady(jumpClient, client);
-    });
   } else {
-    consola.info("Config not found");
+    consola.error("Server config not found");
   }
 }
